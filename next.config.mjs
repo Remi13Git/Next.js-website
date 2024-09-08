ae239eb9a232f3a19ef29f9a11c3b561b13027d1
Remi13Git <remi.hnwi@gmail.com>
2024-09-08 15:42:13 +0200

    update package

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', 
   basePath: '/Next.js-website',
   assetPrefix: '/Next.js-website/'
 };
